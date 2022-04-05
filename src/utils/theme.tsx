8cbedde1a37450fd1e9a0c3f2ccaf4e7cd688c2c
RoanArraes <roan.arraes@hotmail.com>
2022-04-05 17:02:06 -0300

    add new colors

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -2,23 +2,26 @@ const Theme = {
   colors: {
     white: "#FFFF",
     black: "#000",
-    background: "#060234",
-    hover: "#27CBFF",
+    background: "#0B030F",
+    hover: "#9D1FFF",
     menuCircle: "#27CBFF",
     rgba: {
+      white005: "rgba(255, 255, 255, 0.05)",
       white010: "rgba(255, 255, 255, 0.10)",
       black010: "rgba(0, 0, 0, 0.10)",
+      black050: "rgba(0, 0, 0, 0.50)",
     }
   },
-  fonts: ["JuraBold"],
+  fonts: ["JuraBold", "K2dRegular"],
   fontSizes: {
     small: '0.32em',
-    medium: '0.45em',
+    medium: '1em',
     medium1: '1.25em',
     medium2: '1.75em',
-    medium3: '2em',
-    big: '2.5em',
-    extreme: '3.75em'
+    medium3: '3em',
+    medium4: '4em',
+    big: '6em',
+    extreme: '8em'
   },
 }
 
