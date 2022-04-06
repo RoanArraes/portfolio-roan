289959a6aa287d3d716f0cdcead42a2b793e915e
RoanArraes <roan.arraes@hotmail.com>
2022-04-06 10:37:25 -0300

    adjust styles home

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -4,5 +4,6 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   color: ${({theme}) => theme.colors.white};
-  font-size: ${({theme}) => theme.fontSizes.extreme}
+  font-size: ${({theme}) => theme.fontSizes.extreme};
+  background-color: ${({ theme }) => theme.colors.background}
 `;
