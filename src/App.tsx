82a5df580c1afd8ffc9564196061f096c2ba192a
RoanArraes <roan.arraes@hotmail.com>
2022-04-06 11:45:24 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
-
 import Theme from './utils/theme'
-import { Home } from './pages';
+import { About, Home, Projects } from './pages';
+
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={Theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+          <Route path="/projetos" element={<Projects />} />
+        </Routes>
         <Home />
       </ThemeProvider>
     </div>
