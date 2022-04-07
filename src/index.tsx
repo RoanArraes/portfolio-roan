98a1cbf01a42946dbb11af3d76c6905321983c76
RoanArraes <roan.arraes@hotmail.com>
2022-04-07 14:13:39 -0300

    final adjusts

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
-import { createBrowserHistory } from "history";
 
 ReactDOM.render(
   <React.StrictMode>
