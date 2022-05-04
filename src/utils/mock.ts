edf0ba43760099d8a1f2f6644e93af936dd8d862
RoanArraes <roan.arraes@hotmail.com>
2022-05-04 19:45:41 -0300

    adjust link curriculo and stacks

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,7 +1,7 @@
 export const DATA_ABOUT = "Em 2018 iniciei a faculdade de análise e desenvolvimento de sistemas onde ingressei em estágios na área, tive contato com as linguagens Cobol, C#, Java e JavaScript./n A passagem por estas linguagens me mostrou uma maior afinidade com o JavaScript onde tenho maior experiência no front-end utilizando React (atuo com o mesmo desde de 2019)./n Sou uma pessoa criativa, persistente, que sempre busca aprender e aplicar o que for preciso para transformar uma ideia em algo real e funcional.";
 
-export const PRINCIPAL_STACKS = "JavaScript, TypeScript, React, Styled Components, CSS";
+export const PRINCIPAL_STACKS = "JavaScript, TypeScript, React, Styled Components, CSS, Redux, Redux Saga";
 
 export const SENCONDARY_STACKS = "Node, React Native";
 
-export const IN_STUDY = "Jest, Redux";
+export const IN_STUDY = "Jest, Redux, Redux Saga";
